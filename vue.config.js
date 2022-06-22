55ee622358d8d7f13c732d07b4edf61b4b7a8a38
weipy <1203715284@qq.com>
2022-06-22 21:23:46 +0800

    初始化配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 配置开发武器
+  devServer: {
+    // 浏览器自动打开
+    open: true,
+    // 打开地址为本地
+    host: 'localhost'
+  }
 })
